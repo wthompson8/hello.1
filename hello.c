b6bda287ca9e674995c94bee0744a7c076a4c1c1
William Thompson <wthompson8@huskers.unl.edu>
2020-08-18 21:44:54 +0000

    second hack try

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: William Thompson
+ *
+ * Date: 2020/08/17
+ *
+ * A simple hello world program
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+
+  return 0;
+}
